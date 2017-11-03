382dec72a078c493578e2cdbb4ee4c8802aba4a4
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2017-11-02 23:55:11 -0700

    hi

--- ArrayMiniLab/LotsOfCopies.java
+++ ArrayMiniLab/LotsOfCopies.java
@@ -13,22 +13,45 @@ public class LotsOfCopies {
 		changeMe(num, strMain, arrMain);
 		System.out.println(Arrays.toString(arrMain));
 		System.out.println(num);
-		//no change in value when changing the value in the method
-		//b
-		int num2 = num;
-		num = 4;
-		System.out.println(num2);
-		//prints 7
+		System.out.println(strMain);
+		//no change in value when changing the value in the method except for array
+	
+
 	}
 	public static void changeMe(int x, String str, int[] arr) {
+		//2)
+		int b = x;
+		x = 4;
+		System.out.println("b:" + b);
+				//prints 7
+				
+		String bb = str;
+		str = "sup";
+		System.out.println("b:" + bb);
+		//prints APCS
+		
+		int[] newArr = arr;
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i+2;
+		}
+		System.out.println("arr:" + Arrays.toString(newArr));
+		//prints [2, 3, 4, 5, 6] --> yes changes values
+		
+		//1)
 		//can't change values of parameters, because if you input variables from above in first method, comes outs as error
 		//if you change void, just changes what method returns
 		double num = (double) x;
-		double[] arrNum = new double[arr.length];
-		//can't cast String to be something else
+		//doesn't return a number with a different primitive type
+		x = 8;
+		//does change array values
 		for(int i = 0; i < arr.length; i++) {
-			arrNum[i] = arr[i];
+			arr[i] = i;
 		}
+		
+		str = "hello";
+		//can't cast String to be a different type
+		
+		
 	}
 
 }
---
+++ ArraysLab3/ArraysLab3.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+
+//Taylor Nguyen
+//AP Compsci 1st period
+
+public class ArraysLab3 {
+	
+	public static void main(String[] args) {
+		 int[] a1 = {5, 10, 15, 20, 25, 30, 35, 40};
+		 int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
+		 int[] sumArr = sum(a1, a2);
+		 int appendNum = 200;
+		 int[] appendArr = append(a1, appendNum);
+		 int removeIdx = 5;
+		 int[] removeArr = remove(a2, removeIdx);
+		 int sumOfEvens = sumEven(appendArr);
+		 rotateRight(a1);
+		 
+		 System.out.println("sum: "+ Arrays.toString(sumArr));
+		 System.out.println("append: " + Arrays.toString(appendArr));
+		 System.out.println("remove: " + Arrays.toString(removeArr));
+		 System.out.println("even sum: " + sumOfEvens);
+		 System.out.println("rotate right: " + Arrays.toString(a1));
+	}
+	
+	public static int[] sum(int[] arr1, int[] arr2) {
+		int[] newArr = new int[arr1.length];
+		for(int i = 0; i < arr1.length; i++) {
+			int sumOfIndex = arr1[i] + arr2[i];
+			newArr[i] = sumOfIndex;
+		}
+		return newArr;
+	}
+	
+	public static int[] append(int[] arr, int num) {
+		int[] newArr = new int[arr.length + 1];
+		for(int i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i];
+		}
+		newArr[newArr.length-1] = num;
+		return newArr;
+	}
+	
+	public static int[] remove(int[] arr, int idx) {
+		int[] newArr = new int[arr.length - 1];
+		int arrIndex = 0;
+		for(int i = 0; i <= arr.length - 1; i++) {
+			if(arr[arrIndex] != idx) {
+				newArr[i] = arr[arrIndex];
+			} else if(arr[arrIndex] == idx){
+				newArr[i] = arr[arrIndex + 1];
+				arrIndex++;
+			} else if (i == arr.length-1) {
+				append(newArr, arr[i]);
+			}
+			arrIndex++;
+		}
+		return newArr;
+	}
+	
+	public static int sumEven(int[] arr) {
+		int evenSum = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] % 2 == 0) {
+				evenSum += arr[i];
+			}
+		}
+		return evenSum;
+	}
+	
+	public static void rotateRight(int[] arr) {
+		int tempNum = arr[arr.length-1];
+		for(int i = arr.length-1; i >= 1; i--) {
+			arr[i] = arr[i-1];
+		}
+		arr[0] = tempNum;
+	}
+}
