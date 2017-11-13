1c334a01f23f00047247be95e24cbf0f358dde88
nguyen18 <APCS1@MHS-Dreyer-22>
2017-11-13 08:53:37 -0800

    fracalc

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,38 @@
 package fracCalc;
 
+import java.util.Scanner;
+
 public class FracCalc {
 
     public static void main(String[] args) 
     {
+    	boolean done = false;
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	while(!done) {
+    		Scanner scans = new Scanner(System.in);
+    		System.out.println("please input your equation (ex: 1/2 + 1/2)");
+    		String equation = scans.nextLine();
+    		System.out.println(produceAnswer(equation));
+    		
+    		System.out.println("are you done? type quit to exit or no to continue");
+    		String answer = scans.nextLine();
+    		if(answer.equals("no")) {
+    			done = false;
+    		} else if (answer.equals("quit")) {
+    			done = true;
+    		} 
+    		while (!answer.equals("quit") && !answer.equals("no")) {
+    		System.out.println("try again. type quit or no"); 
+    		answer = scans.nextLine();
+    		if(answer.equals("no")) {
+    			done = false;
+    		} else if (answer.equals("quit")) {
+    			done = true;
+    		} 
+    		}
+    	}
+    
+ 
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -20,7 +47,14 @@ public class FracCalc {
     { 
         // TODO: Implement this function to produce the solution to the input
         
-        return "";
+    	String[] separatedOperands = input.split(" ");
+		String operand2 = separatedOperands[2];
+		String[] operand2Split = operand2.split("_");
+		operand2Split = operand2.split("/");
+		
+		
+		return operand2;
+        
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
