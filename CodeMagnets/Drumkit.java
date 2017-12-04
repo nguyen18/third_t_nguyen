ac82d27679359096c838594eee92adc424fd6c2e
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2017-12-04 01:56:48 -0800

    jkh

---
+++ CodeMagnets/Drumkit.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Drumkit {
+	public static void main(String[] args) {
+		System.out.print(5- 6);
+		String[] hello = "hello".split("e");
+		System.out.print(hello[1]);
+	}
+}
--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class FracCalc {
 
+	//main method is basically the client
     public static void main(String[] args) 
     {
     	boolean done = false;
