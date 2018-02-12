40bfbac76663d4fb835ae919e023537547447731
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2018-02-12 01:00:34 -0800

    yeah man

--- Prisms/RectangularPrism.java
+++ Prisms/RectangularPrism.java
@@ -1,5 +1,5 @@
 /* Write this concrete (non-abstract) class called RectangularPrism.  
- * It has 2 private fields (length and width).
+ * It has 2 private fields (length and width). yay
  * It inherits its height from its superclass, Prism (because all prisms have a height).
  * Provide a constructor and the methods required by its abstract superclass.  
  */
